3592309f547125ca651df7245e1a1239730bf754
vrkljam <vrkljam@earthlink.net>
2024-07-12 11:25:34 -0600

    fix update user

--- controllers/userControllers.js
+++ controllers/userControllers.js
@@ -2,7 +2,10 @@ import { StatusCodes } from "http-status-codes";
 import User from "../models/UserModel.js";
 import Job from "../models/JobModel.js";
 import cloudinary from "cloudinary";
-import { promises as fs } from "fs";
+import { formatImage } from "../middleware/multerMiddleware.js";
+
+// >>>>> removed this import promise bc vid 183 <<<<
+// import { promises as fs } from "fs";
 
 export const getCurrentUser = async (req, res) => {
   const user = await User.findOne({ _id: req.user.userId });
@@ -20,8 +23,13 @@ export const updateUser = async (req, res) => {
   const newUser = { ...req.body };
   delete newUser.password;
   if (req.file) {
-    const response = await cloudinary.v2.uploader.upload(req.file.path);
-    await fs.unlink(req.file.path);
+    const file = formatImage(req.file);
+
+    const response = await cloudinary.v2.uploader.upload(file);
+
+    // >>>>  removed this line of code below bc changing to datauri, see vid #183 <<<<
+    // await fs.unlink(req.file.path);
+
     newUser.avatar = response.secure_url;
     newUser.avatarPublicId = response.public_id;
   }
