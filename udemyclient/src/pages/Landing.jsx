de1e65185f30f27f7623021483cb3d8294c1240f
vrkljam <vrkljam@earthlink.net>
2024-07-12 09:31:38 -0600

    typos fixed on landing page

--- udemyclient/src/pages/Landing.jsx
+++ udemyclient/src/pages/Landing.jsx
@@ -1,11 +1,11 @@
 import React from "react";
-import styled from "styled-components";
 import Wrapper from "../assets/wrappers/LandingPage";
 import main from "../assets/images/main.svg";
 import { Link } from "react-router-dom";
-import Logo from "../components/Logo";
+import { Logo } from "../components";
 
 // video did this one first then did wrapper later in video
+// import styled from "styled-components";
 // const StyledBtn = styled.button`
 //   font-size: 1.5rem;
 //   background: red;
@@ -19,22 +19,20 @@ const Landing = () => {
         <Logo />
       </nav>
       <div className="container page">
-        <h1>
-          Job<span>Tracking</span>App
-        </h1>
-        <p>
-          Lorem ipsum dolor sit, amet consectetur adipisicing elit. Impedit ipsa
-          praesentium velit enim, optio porro modi aliquid placeat, quia est ad
-          sunt facere commodi! Odio excepturi doloremque est vero voluptas!
-        </p>
-        <Link to="/register" className="btn register-link">
-          Register
-        </Link>
-        <Link to="/login" className="btn">
-          Login/ Demo User
-        </Link>
+        <div className="info">
+          <h1>
+            Job<span>Tracking</span>App
+          </h1>
+          <p>This site helps you to track your job search journey.</p>
+          <Link to="/register" className="btn register-link">
+            Register
+          </Link>
+          <Link to="/login" className="btn">
+            Login/ Demo User
+          </Link>
+        </div>
+        <img src={main} alt="job hunt" className="img main-img" />
       </div>
-      <img src={main} alt="job hunt" className="img main-img" />
     </Wrapper>
   );
 };
