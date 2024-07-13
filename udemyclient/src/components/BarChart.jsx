81dc4e2e020355928caaeea54320aa65f0573bc5
vrkljam <vrkljam@earthlink.net>
2024-07-13 14:34:24 -0600

    fixed barchart date 	modified:   udemyclient/src/components/BarChart.jsx

--- udemyclient/src/components/BarChart.jsx
+++ udemyclient/src/components/BarChart.jsx
@@ -2,20 +2,20 @@ import React from "react";
 import {
   BarChart,
   Bar,
-  XAxis,
   YAxis,
   CartesianGrid,
   Tooltip,
   ResponsiveContainer,
+  XAxis,
 } from "recharts";
 
 const BarChartComponent = ({ data }) => {
   return (
     <ResponsiveContainer width="100%" height={300}>
-      <BarChart data={data} margin={{ top: 50 }}>
+      <BarChart data={data} margin={{ top: 100 }}>
         <CartesianGrid strokeDasharray="3 3" />
         <XAxis dataKey="date" />
-        <YAxis allowDecimals={false} />
+        <YAxis dataKey="count" allowDecimals={false} />
         <Tooltip />
         <Bar dataKey="count" fill="#2cb1bc" barSize={75} />
       </BarChart>
@@ -24,3 +24,28 @@ const BarChartComponent = ({ data }) => {
 };
 
 export default BarChartComponent;
+
+// import {
+//   BarChart,
+//   Bar,
+//   XAxis,
+//   YAxis,
+//   CartesianGrid,
+//   Tooltip,
+//   ResponsiveContainer,
+// } from "recharts";
+
+// const BarChartComponent = ({ data }) => {
+//   return (
+//     <ResponsiveContainer width="100%" height={300}>
+//       <BarChart data={data} margin={{ top: 50 }}>
+//         <CartesianGrid strokeDasharray="3 3" />
+
+//         <YAxis allowDecimals={false} dataKey="count" />
+//         <XAxis dataKey="date" />
+//         <Tooltip />
+//         <Bar dataKey="count" fill="#2cb1bc" barSize={75} />
+//       </BarChart>
+//     </ResponsiveContainer>
+//   );
+// };
